d213f63dc4b5863b8d007d37ba9a60be70bc8995
Andy Wang <yagosys@gmail.com>
2024-03-18 17:56:28 +0800

    fix alicloud provider version

--- fortigate_deployment/fortigate/provider.tf
+++ fortigate_deployment/fortigate/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    alicloud = {
+      source  = "hashicorp/alicloud"
+      version = "1.211.2"
+    }
+  }
+}
+
 provider "alicloud" {
         profile = var.profile 
         region  = var.region
